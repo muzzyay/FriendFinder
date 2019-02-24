fc3dce9ffe0262e610977b08e7dd9abb1fdba9cb
muzzyay <muzzyay@gmail.com>
2019-02-24 13:22:42 -0700

    Done

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends = [
     {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Naomi",
+        "photo": "http://lorempixel.com/output/people-h-c-199-213-9.jpg",
         "scores": [
             5,
             1,
@@ -13,6 +13,51 @@ var friends = [
             5,
             4,
             1
+        ]
+    }, {
+        "name": "Elly",
+        "photo": "http://lorempixel.com/output/people-h-c-199-213-10.jpg",
+        "scores": [
+            3,
+            4,
+            2,
+            2,
+            3,
+            4,
+            5,
+            3,
+            4,
+            1
+        ]
+    }, {
+        "name": "John",
+        "photo": "http://lorempixel.com/output/people-h-c-199-213-6.jpg",
+        "scores": [
+            5,
+            2,
+            3,
+            3,
+            1,
+            4,
+            2,
+            3,
+            3,
+            5
+        ]
+    },{
+        "name": "Arthur",
+        "photo": "http://lorempixel.com/output/people-h-c-199-213-7.jpg",
+        "scores": [
+            4,
+            2,
+            3,
+            5,
+            1,
+            3,
+            4,
+            2,
+            1,
+            5
         ]
     }];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,12 +10,35 @@ module.exports = function (app, friends) {
         // req.body hosts is equal to the JSON post sent from the user
         // This works because of our body parsing middleware
         var newFriend = req.body;
+        var theMatch;
+        var leastDifference = 40;
 
         console.log(newFriend);
 
+        friends.forEach(element => {
+            var totalDifference = 0;
+
+            for (var i = 0; i < newFriend.scores.length; i++) {
+
+                var num1 = newFriend.scores[i];
+                var num2 = element.scores[i];
+
+                var singleDifference = Math.abs(num1 - num2);
+
+                totalDifference = singleDifference + totalDifference;
+               
+            }
+            console.log(totalDifference);
+
+            if (totalDifference < leastDifference) {
+                leastDifference = totalDifference;
+                theMatch = element;
+            }
+        });
+
         friends.push(newFriend);
 
-        res.json(newFriend);
+        res.json(theMatch);
     });
 
 }
